3e4c5bb72f0851103aed95d9f1ba052397d3f571
Personal <sk7627577@gmail.com>
2026-04-10 15:49:47 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
-  
+  experimental: {
+    esmExternals: true, // helps prefer modern builds
+    optimizePackageImports: ["lodash", "date-fns", "react-icons"],
+    optimizeCss: true,
+    optimizeServerReact: true,
+  },
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
