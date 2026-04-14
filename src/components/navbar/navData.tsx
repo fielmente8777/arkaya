667c8b075460c5a0f581fb065eb01a1ce1de569d
Personal <sk7627577@gmail.com>
2026-04-14 15:08:35 +0530

    up

--- src/components/navbar/navData.tsx
+++ src/components/navbar/navData.tsx
@@ -7,7 +7,7 @@ export const navUpper = {
     href: contact.addressLink,
     icon: <FillLocationIcon />,
   },
-  temperature: "29.396657201082572, 79.6496019576717",
+  temperature: "29.396886, 79.649651",
   // temperature: "29.146027515778144, 75.70353074035758", // test
 };
 
